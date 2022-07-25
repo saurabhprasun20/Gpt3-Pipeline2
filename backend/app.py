7a86063f360e4e06c85007ae84f7f8f097c0c075
tanmaychimurkar <tanmay.chimurkar@uzh.ch>
2022-07-25 23:20:46 +0200

    adding changes to compute time of the api

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request
-from gpt3 import answer_gpt3
-from In_out_DomainClassification import user_input
 import json
+import time
+from flask import Flask, request, g as app_ctx
+from In_out_DomainClassification import user_input
+import logging
+import ast
 
-# from In_out_DomainClassification import user_input
-# from fa_chat_close import genResults
-# from fa_chat_close import getBertAnswer
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s:[%(filename)s:%(lineno)d] - %(message)s [%(asctime)s]',
+                    datefmt='%H:%M:%S')
 
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
 
 app = Flask(__name__)
 
@@ -17,13 +21,31 @@ def hello_world():
     return 'Hello Juli!'
 
 
+@app.before_request
+def logging_before():
+    app_ctx.start_time = time.perf_counter()
+
+
+@app.after_request
+def logging_after(response):
+    total_time = time.perf_counter() - app_ctx.start_time
+    time_in_ms = int(total_time * 1000)
+    LOGGER.info(f'The total execution time for the request is: {time_in_ms} ms {request.method}')
+    return response
+
+
 @app.route('/api/v1/answer', methods=['POST'])
 def get_gpt3_answer():
     record = json.loads(request.data)
     print(record['answer']['question'])
     inp = [record['answer']['question']]
     answer_returned = user_input(inp)
-    # print(token)
+    LOGGER.info(f'Answer returned is {answer_returned}, {type(answer_returned)}')
+    if "[" in answer_returned:
+        print('true, this shit is happening dawg')
+        answer_returned = ast.literal_eval(answer_returned.strip())
+        print(f'{answer_returned}')
+        answer_returned = answer_returned[0]
     return answer_returned, 200
 
 
