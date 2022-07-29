68abc27f0863d337505461a0aefd6b88e335e468
tanmaychimurkar <tanmay.chimurkar@uzh.ch>
2022-07-29 20:06:49 +0200

    removing commented print statement

--- backend/app.py
+++ backend/app.py
@@ -41,26 +41,12 @@ def logging_after(response):
     LOGGER.info(f'The total execution time for the request is: {time_in_ms} ms {request.method}')
     LOGGER.info(f'The input is {input_global} and the answer is {answer_global}')
 
-    # conn = psycopg2.connect(
-    #     host='localhost',
-    #     database='julidata',
-    #     user='uzh_admin',
-    #     password='password'
-    # )
-
-    # cur = conn.cursor()
     if flag == 0:
-        # cur.execute('INSERT INTO davinci_avg (question, answer, response_time)'
-        #             'values (%s, %s, %s)',
-        #             (input_global[0], answer_global, time_in_ms))
-        db.davinci_avg.insert_one({'question': input_global[0], 'answer': answer_global,"time": time_in_ms})
+        db.davinci_avg.insert_one({'question': input_global[0], 'answer': answer_global, "time": time_in_ms})
 
     else:
-        # cur.execute('INSERT INTO curie_avg (question, answer, response_time)'
-        #             'values (%s, %s, %s)',
-        #             (input_global[0], answer_global, time_in_ms))
-        db.curie_avg.insert_one({'question': input_global[0], 'answer': answer_global,"time": time_in_ms})
- 
+        db.curie_avg.insert_one({'question': input_global[0], 'answer': answer_global, "time": time_in_ms})
+
     return response
 
 
